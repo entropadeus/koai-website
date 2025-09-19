755174b35400bc81d6f4a1dc519099018140af53
KOAI Engineer <engineer@koai.studio>
2025-09-19 08:01:19 -0500

    chore: enable static export for github pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const REPO_NAME = 'koai-website';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   experimental: {
     typedRoutes: true
-  }
+  },
+  ...(isProd
+    ? {
+        basePath: `/${REPO_NAME}`,
+        assetPrefix: `/${REPO_NAME}/`
+      }
+    : {})
 };
 
 module.exports = nextConfig;
-
-
